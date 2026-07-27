8bb2438512ca6f8e6ca5bcd4441e1d2f1812c73b
Emma Hare <emsophiehare@gmail.com>
2026-07-27 11:30:44 +0200

    Set up DesignDesk project

---
+++ src/main/java/com/emmahare/designdesk/DesigndeskApplication.java
@@ -0,0 +1,13 @@
+package com.emmahare.designdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesigndeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesigndeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmahare/designdesk/DesigndeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmahare.designdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesigndeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
