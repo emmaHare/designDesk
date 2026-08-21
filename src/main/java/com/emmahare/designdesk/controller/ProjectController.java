dd76275cf2dda60096f7359959167a7690490747
Emma Hare <emsophiehare@gmail.com>
2026-08-21 13:25:37 +0200

    Create new project form, closes #17

--- src/main/java/com/emmahare/designdesk/controller/ProjectController.java
+++ src/main/java/com/emmahare/designdesk/controller/ProjectController.java
@@ -1,21 +1,22 @@
 package com.emmahare.designdesk.controller;
 
 import com.emmahare.designdesk.model.Project;
+import com.emmahare.designdesk.service.ClientService;
 import com.emmahare.designdesk.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final ClientService clientService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, ClientService clientService) {
         this.projectService = projectService;
+        this.clientService = clientService;
     }
 
     @GetMapping
@@ -31,5 +32,20 @@ public class ProjectController {
         model.addAttribute("project", project);
 
         return "projects/details";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("project", new Project());
+        model.addAttribute("clients", clientService.findAll());
+
+        return "projects/new";
+    }
+
+    @PostMapping
+    public String createProject(@ModelAttribute Project project) {
+        projectService.save(project);
+
+        return "redirect:/projects";
     }
 }
