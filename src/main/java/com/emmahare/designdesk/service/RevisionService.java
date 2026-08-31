82bc2e09b3ec24fa6de43ff2b28c7a16a26e9fad
Emma Hare <emsophiehare@gmail.com>
2026-08-31 11:45:20 +0200

    Create RevisionService, closes #32

---
+++ src/main/java/com/emmahare/designdesk/service/RevisionService.java
@@ -0,0 +1,29 @@
+package com.emmahare.designdesk.service;
+
+import com.emmahare.designdesk.model.Revision;
+import com.emmahare.designdesk.repository.RevisionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RevisionService {
+
+    private final RevisionRepository revisionRepository;
+
+    public RevisionService(RevisionRepository revisionRepository) {
+        this.revisionRepository = revisionRepository;
+    }
+
+    public List<Revision> findAll() {
+        return revisionRepository.findAll();
+    }
+
+    public Revision save(Revision revision) {
+        return revisionRepository.save(revision);
+    }
+
+    public void deleteById(Long id) {
+        revisionRepository.deleteById(id);
+    }
+}
